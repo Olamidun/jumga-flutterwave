55d8e95a2b83767e5785199239e52a5a5a64ba15
Tofunmi Osho <oshotofunmi@gmail.com>
2021-01-12 15:23:45 +0100

    add customers' orders

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,23 +1,23 @@
 // declare variables
 const hamburger = document.querySelector('.hamburger');
 const sidebar = document.querySelector('.sidebar');
-const modal = document.querySelector('.modal');
+const backdrop = document.querySelector('.backdrop');
 let open = false;
 
 // functions
 const toggleSidebar = () => {
   if(!open) {
     sidebar.style.transform = 'translateX(0)';
-    modal.style.display = 'block';
+    backdrop.style.display = 'block';
     open = true;
   } else {
     sidebar.style.transform = 'translateX(-300px)';
-    modal.style.display = 'none';
+    backdrop.style.display = 'none';
     open = false;
   }
 }
 
 // event listeners
 hamburger.addEventListener('click', toggleSidebar);
-modal.addEventListener('click', toggleSidebar);
+backdrop.addEventListener('click', toggleSidebar);
 sidebar.addEventListener('click', toggleSidebar);
